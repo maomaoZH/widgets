01ffeeb2741093d9d3185946ebccdefca3d7146a
Maomao Meyer-Zhang <maomao.meyer-zhang@telekom.de>
2022-05-04 20:59:59 +0200

    first commit

---
+++ stencil.config.ts
@@ -0,0 +1,22 @@
+import { Config } from '@stencil/core';
+
+export const config: Config = {
+  namespace: 'widget',
+  globalStyle: 'src/global/scale.css',
+  outputTargets: [
+    {
+      type: 'dist',
+      esmLoaderPath: '../loader',
+    },
+    {
+      type: 'dist-custom-elements',
+    },
+    {
+      type: 'docs-readme',
+    },
+    {
+      type: 'www',
+      serviceWorker: null, // disable service workers
+    },
+  ],
+};
